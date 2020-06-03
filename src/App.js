e6855879297baa01c3c00cb6067f4e5251f0d4e7
David Clark <david.e.clark@ntlworld.com>
2020-06-03 23:53:07 +0100

    app fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-import Quiz from "./Quiz.js";
+import Quiz from  "./Quiz.js";
 
 class App extends Component {
   render() {
-    return <Quiz />;
+    return (<div /> <Quiz />)
   }
 }
 
